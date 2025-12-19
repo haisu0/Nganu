8b805ed5d2295afc9fbfd3c14a9736d168adeabb
haisu0 <hai897191@gmail.com>
2025-12-19 13:27:41 +0700

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,6 +1,6 @@
 import { auth, db } from "./firebase.js";
 import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
-import { doc, getDoc } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { collection, getDocs, query, where } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 const emailInput = document.getElementById("email");
 const passwordInput = document.getElementById("password");
@@ -12,18 +12,18 @@ window.login = async () => {
   try {
     await signInWithEmailAndPassword(auth, email, password);
 
-    const adminDoc = await getDoc(doc(db, "admins", "adminAccount"));
-    const viewerDoc = await getDoc(doc(db, "admins", "viewerAccount"));
+    // Cari role di Firestore
+    const q = query(collection(db,"admins"), where("email","==",email));
+    const snap = await getDocs(q);
+    if(snap.empty) return alert("Email belum terdaftar di Firestore");
 
-    if (email === adminDoc.data().email) {
-      window.location.href = "admin.html";
-    } else if (email === viewerDoc.data().email) {
-      window.location.href = "feed.html";
-    } else {
-      alert("Email tidak terdaftar");
-    }
+    const role = snap.docs[0].data().role;
 
-  } catch (err) {
+    if(role === "atmin") window.location.href = "admin.html";
+    else if(role === "bukanatmin") window.location.href = "feed.html";
+    else alert("Role tidak valid");
+
+  } catch(err) {
     alert("Login gagal: " + err.message);
   }
 };
